611a8b95798ae2cc8b11880dc98d3f632db59336
yunhaoguo <929394263@qq.com>
2019-02-12 22:35:30 -0500

    add tab bar and get location

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { Tabs, Button } from 'antd';
+import { GEO_OPTIONS } from "../constants";
+
+
+const TabPane = Tabs.TabPane;
 
 export class Home extends React.Component {
+
+    componentDidMount() {
+        if ('geolocation' in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                this.onSuccessLoadGeoLocation,
+                this.onFailedLoadGeoLocation,
+                GEO_OPTIONS,
+            );
+        } else {
+
+        }
+    }
+
+    onSuccessLoadGeoLocation = (position) => {
+
+    }
+
+    onFailedLoadGeoLocation = (error) => {
+
+    }
+
     render() {
+        const operations = <Button type="primary">Create New Post</Button>;
         return (
-            <div>This is Home!</div>
+            <Tabs className="main-tabs" tabBarExtraContent={operations}>
+                <TabPane tab="Image Posts" key="1">Content of tab 1</TabPane>
+                <TabPane tab="Video Posts" key="2">Content of tab 2</TabPane>
+                <TabPane tab="Map" key="3">Content of tab 3</TabPane>
+            </Tabs>
         );
     }
 }
+
+
+
